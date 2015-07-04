f7dc9bc49dc166af0f5b2bdf5bd0d0514ff3cdce
Boopathi Rajaa <me@boopathi.in>
2015-07-04 21:54:09 +0530

    Bootstrap stuff and initial working code

---
+++ numberify.go
@@ -0,0 +1,31 @@
+package numberify
+
+import (
+  "strconv"
+  "strings"
+  "regexp"
+)
+
+func Numberify(str string) string {
+  words := strings.Fields(str)
+  rp := regexp.MustCompile("\\w+")
+  tokens := make([]string, 0)
+  for _, word := range words {
+    word = rp.ReplaceAllStringFunc(word, func(s string) string {
+      var (
+        fst string
+        lst string
+        occ int
+      )
+      if (len(s) < 2) {
+        return s
+      }
+      fst = string([]rune(s)[0])
+      lst = string([]rune(s)[len(s)-1])
+      occ = len(s) - 2
+      return fst + strconv.Itoa(occ) + lst
+    })
+    tokens = append(tokens, word)
+  }
+  return strings.Join(tokens, " ")
+}
---
+++ numberify/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+  "os"
+  "fmt"
+  "github.com/boopathi/numberify"
+  "github.com/codegangsta/cli"
+)
+
+func Action(c *cli.Context) {
+  fmt.Println(numberify.Numberify("Text to check with a \"quot\""))
+}
+
+func main() {
+  app := cli.NewApp()
+  app.Name = "numberify"
+  app.Usage = "numberify \"text to numberify\""
+  app.Action = Action
+  app.Run(os.Args)
+}
