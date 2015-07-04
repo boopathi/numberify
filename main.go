80094577b4dcebf29c985dde67525b27c889fed8
Boopathi Rajaa <me@boopathi.in>
2015-07-04 22:12:36 +0530

    Format code gofmt

--- main.go
+++ main.go
@@ -1,29 +1,29 @@
 package main
 
 import (
-  "os"
-  "fmt"
-  "github.com/boopathi/numberify/numberify"
-  "github.com/codegangsta/cli"
-  "bufio"
+	"bufio"
+	"fmt"
+	"github.com/boopathi/numberify/numberify"
+	"github.com/codegangsta/cli"
+	"os"
 )
 
 func Action(c *cli.Context) {
-  reader := bufio.NewReader(os.Stdin)
+	reader := bufio.NewReader(os.Stdin)
 
-  for {
-    line, err := reader.ReadString('\n')
-    if err != nil {
-      break
-    }
-    fmt.Println(numberify.Numberify(string(line)))
-  }
+	for {
+		line, err := reader.ReadString('\n')
+		if err != nil {
+			break
+		}
+		fmt.Println(numberify.Numberify(string(line)))
+	}
 }
 
 func main() {
-  app := cli.NewApp()
-  app.Name = "numberify"
-  app.Usage = "numberify \"text to numberify\""
-  app.Action = Action
-  app.Run(os.Args)
+	app := cli.NewApp()
+	app.Name = "numberify"
+	app.Usage = "numberify \"text to numberify\""
+	app.Action = Action
+	app.Run(os.Args)
 }
